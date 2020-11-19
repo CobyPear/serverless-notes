2380d4f6f25ca380fa8b2830a2cf539e5540e486
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-19 16:10:46 -0600

    update lambda and route

---
+++ notes-api/update.js
@@ -0,0 +1,32 @@
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: '123',
+            noteId: event.pathParameters.id,
+        },
+        /*
+            UpdateExpression defines attributes to be updated
+            ExpressionAttributeValues defines value in the update expression
+        */
+        UpdateExpression: 'SET content = :content, attachment = :attachment',
+        ExpressionAttributeValues: {
+            ':attachment': data.attachment || null,
+            ':content': data.content || null,
+        },
+        /*
+            ReturnValues specifies if and how to return the items' attributes,
+            where ALL_NEW returns all attributes of the item after the update;
+            you can inspect 'result' below to see how it works with different settings
+        */
+       ReturnValues: 'ALL_NEW',
+    };
+
+    await dynamoDb.update(params);
+
+    return { status: true };
+});
